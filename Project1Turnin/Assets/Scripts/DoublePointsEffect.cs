6075c67a376c659e87a30846aaca9bb192f6d064
svc-student <svc-student@student.directory.jccc>
2019-02-13 13:49:02 -0600

    adding turnin project

---
+++ Project1Turnin/Assets/Scripts/DoublePointsEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoublePointsEffect : MonoBehaviour
+{
+    public float duration = 5;
+    PlayerScore myScore;
+
+    private void Start()
+    {
+        myScore = GetComponent<PlayerScore>();
+        myScore.modifier *= 2;
+        Destroy(this, duration);
+    }
+
+    private void OnDestroy()
+    {
+        myScore.modifier /= 2;
+    }
+}
---
+++ Project1Turnin/Assets/Scripts/DoublePointsPowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoublePointsPowerUp : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerScore>() != null)
+        {
+            other.gameObject.AddComponent<DoublePointsEffect>();
+        }
+    }
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.GetComponent<PlayerScore>() != null)
+    {
+      collision.gameObject.AddComponent<DoublePointsEffect>();
+    }
+  }
+}
