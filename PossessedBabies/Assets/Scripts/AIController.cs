97a45d76b59dbb0e2883a2034677c191fff80268
svc-student <svc-student@student.directory.jccc>
2019-02-25 20:11:06 -0600

    init

---
+++ PossessedBabies/Assets/Scripts/AIController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AIState
+{
+  Patrol, Arrive, Attack, Hover, ReturnBaby
+}
+
+public class AIController : MonoBehaviour
+{
+  public AIState aiState;
+
+  private void Update()
+  {
+    switch (aiState)
+    {
+      case AIState.Patrol:
+        PatrolLogic();
+        break;
+      case AIState.Arrive:
+        ArriveLogic();
+        break;
+      case AIState.Attack:
+        AttackLogic();
+        break;
+      case AIState.Hover:
+        HoverLogic();
+        break;
+      case AIState.ReturnBaby:
+        ReturnBabyLogic();
+        break;
+    }
+  }
+
+  private void PatrolLogic()
+  {
+
+  }
+
+  private void ArriveLogic()
+  {
+
+  }
+
+  private void AttackLogic()
+  {
+
+  }
+
+  private void HoverLogic()
+  {
+
+  }
+
+  private void ReturnBabyLogic()
+  {
+
+  }
+}
---
+++ PossessedBabies/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+  public float healthMax;
+
+  private float _health;
+  public float Health
+  {
+    get
+    {
+      return _health;
+    }
+    set
+    {
+      if (value > healthMax)
+        _health = healthMax;
+      else if (value < 0)
+        Die();
+      else
+        _health = value;
+    }
+  }
+
+
+  public void Die()
+  {
+
+  }
+}
